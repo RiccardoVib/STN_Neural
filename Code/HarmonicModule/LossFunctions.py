f2959abcb94f36772fd43355aec272fcb5de8538
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-05-29 11:46:59 +0200

    Update LossFunctions.py

--- Code/HarmonicModule/LossFunctions.py
+++ Code/HarmonicModule/LossFunctions.py
@@ -28,19 +28,13 @@ class STFT_loss(tf.keras.losses.Loss):
 
         y_true = tf.squeeze(y_true)
         y_pred = tf.squeeze(y_pred)
-        y_true = tf.reshape(y_true, [-1, self.m[0]])
-        y_pred = tf.reshape(y_pred, [-1, self.m[0]])
+        y_true = tf.reshape(y_true, [1, -1]])
+        y_pred = tf.reshape(y_pred, [1, -1])
         loss = 0
         for i in range(len(self.m)):
 
-            pad_amount = int((self.m[i]-self.m[0])//2)  # Symmetric even padding like librosa.
-            pads = [[0, 0] for _ in range(len(y_true.shape))]
-            pads[1] = [pad_amount, pad_amount]
-            y_true = tf.pad(y_true, pads, mode='CONSTANT', constant_values=0)
-            y_pred = tf.pad(y_pred, pads, mode='CONSTANT', constant_values=0)
-
-            Y_true = K.abs(tf.signal.stft(y_true, frame_length=self.m[i], frame_step=self.m[i] // 4, fft_length=self.m[i], pad_end=False))
-            Y_pred = K.abs(tf.signal.stft(y_pred, frame_length=self.m[i], frame_step=self.m[i] // 4, fft_length=self.m[i], pad_end=False))
+            Y_true = K.abs(tf.signal.stft(y_true, frame_length=self.m[i], frame_step=self.m[i] // 4, pad_end=True))
+            Y_pred = K.abs(tf.signal.stft(y_pred, frame_length=self.m[i], frame_step=self.m[i] // 4, pad_end=True))
 
             Y_true = K.pow(Y_true, 2)
             Y_pred = K.pow(Y_pred, 2)
