51bd1451b454282937312aa1c884af76b2e936e0
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-09-11 10:43:32 +0200

    Add files via upload

--- Code/HarmonicModule/DatasetsClass.py
+++ Code/HarmonicModule/DatasetsClass.py
@@ -1,47 +1,110 @@
 import pickle
 import os
+import scipy
+from Utils import AttTime
+import librosa
 import numpy as np
 from tensorflow.keras.utils import Sequence
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 
 class DataGeneratorPickles(Sequence):
 
-    def __init__(self, data_dir, set, steps, model, batch_size=2800, type=np.float64):
-
-        data = open(os.path.normpath('/'.join([data_dir, 'DatasetSingleNote_split.pickle'])), 'rb')
+    def __init__(self, filename, data_dir, set, steps, model, batch_size=2800, minibatch_size=2400, stage='', type=np.float64):
+        """
+        Initializes a data generator object
+          :param data_dir: the directory in which data are stored
+          :param output_size: output size
+          :param batch_size: The size of each batch returned by __getitem__
+        """
+        data = open(os.path.normpath('/'.join([data_dir, filename + '.pickle'])), 'rb')
         Z = pickle.load(data)
-        y, keys, velocities, partials, f0, B, S, T, N, DCT, attackTimes = Z[set]
+        y, keys, velocities, partials, f0, B, amps = Z[set]
+        #if set == 'train':
+        #    y_v, _, velocities_v, _, _, _, amps_v = Z['val']
+        #    y = np.concatenate([y, y_v], axis=0)
+        #    velocities = np.concatenate([velocities, velocities_v], axis=0)
+        #    amps = np.concatenate([amps, amps_v[np.newaxis]], axis=0)
+
+        ### add
+        S = np.zeros((y.shape[0], y.shape[1]))
+        #attackTimes = np.zeros((y.shape[0]))
+        for i in range(y.shape[0]):
+            D = librosa.stft(y[i])
+            D_harmonic, D_percussive = librosa.decompose.hpss(D, margin=10)
+            harmonic = librosa.istft(D_harmonic)
+            harmonic = np.pad(np.array(harmonic, dtype=type), [0, len(y[0]) - len(harmonic)])
+            S[i] = harmonic
+            #attackTime = AttTime(S[i])
+            #attackTimes[i] = attackTime
+
+        self.stage = stage
+        self.filename = filename
         self.batch_size = batch_size
-        self.y = np.array(y, dtype=type)#336000
-        self.S = np.array(S, dtype=type)#336000
-        self.ratio = y.shape[1] // steps
+        self.minibatch_size = minibatch_size
+        self.steps = steps
+        self.y = np.array(y, dtype=type)
+        w = scipy.signal.windows.tukey(self.batch_size, alpha=0.000005, sym=True).reshape(1,-1)
+        self.S = np.array(S[:, :self.batch_size], dtype=type)*w
+        self.ratio = self.S.shape[1] // (steps)
+
+        #lim = self.ratio//self.batch_size*self.batch_size
+
+        if len(amps.shape) < 3:
+            amps = amps[np.newaxis]
+        self.amps = np.zeros((self.S.shape[0], self.S.shape[1], 6))
+
+
+        amps0 = np.zeros((amps.shape[0], amps.shape[2]))
+        step = (amps[:, 1, :] - amps0) / 32
+        for j in range(32):
+            #print(j)
+            self.amps[:, j, :] = amps0 + amps0 + step * j
+
+        for i in range(1, amps.shape[1]):
+            step = (amps[:, i, :] - amps[:, i-1, :]) / 32
+            for j in range(32):
+                t = 32*i+j
+                #print(t)
+                self.amps[:, t, :] = amps[:, i-1, :] + amps[:, i-1, :]*step*j
+            if t == self.amps.shape[1]-1:
+                break
+
+        self.amps = self.amps.reshape(-1, minibatch_size, self.steps, 6)
 
         ###metadata
         self.f0 = f0.reshape(-1, 1)
-        self.velocities = velocities.reshape(-1, 1)
+        self.velocities = velocities.reshape(-1, 1)/111
         self.partials = partials.reshape(-1, 6)
         self.B = B
-        self.attackTimes = np.array(attackTimes, dtype=type).reshape(-1, 1)
-        self.n_note = self.f0.shape[0]
+        #self.attackTimes = np.array(attackTimes, dtype=type).reshape(-1, 1)
+        self.n_note = self.velocities.shape[0]
 
         #########
 
         # waveforms
         self.k = np.arange(0, self.ratio).reshape(-1, 1)
         self.k = np.array(self.k, dtype=np.float32)
-        self.k = np.repeat(self.k.T, self.n_note, axis=0).reshape(-1, 1)
+        self.k = np.repeat(self.k.T, self.n_note, axis=0).reshape(-1, self.minibatch_size, steps)
 
-        self.y = self.y.reshape(-1, steps)
-        self.S = self.S.reshape(-1, steps)
+        #self.y = self.y.reshape(-1, minibatch_size, steps)
+        self.S = self.S.reshape(-1, minibatch_size, steps)
 
-        self.rms = np.abs(tf.reduce_mean(np.square(self.S), axis=-1)).reshape(-1, 1)
-        self.alfas = np.max(np.abs(self.S), axis=-1).reshape(-1, 1)
+        self.rms = np.abs(tf.reduce_mean(np.square(self.S), axis=-1)).reshape(-1, minibatch_size, 1)
+        self.alfas = np.max(np.abs(self.S), axis=-1).reshape(-1, minibatch_size, 1)
 
-        self.f0 = np.repeat(self.f0, self.ratio, axis=0)
-        self.partials = np.repeat(self.partials, self.ratio, axis=0)
-        self.velocities = np.repeat(self.velocities, self.ratio, axis=0).reshape(-1, 1)
-        self.B = np.repeat(self.B, self.ratio, axis=0).reshape(-1, 1)
-        self.attackTimes = np.repeat(self.attackTimes, self.ratio, axis=0).reshape(-1, 1)
+        self.f0 = np.repeat(self.f0, self.ratio*self.n_note, axis=0).reshape(-1, minibatch_size, 1)
+        self.partials = np.repeat(self.partials, self.ratio*self.n_note, axis=0).reshape(-1, minibatch_size, 6)
+        self.velocities = np.repeat(self.velocities, self.ratio, axis=0).reshape(-1, minibatch_size, 1)
+        self.B = np.repeat(self.B, self.ratio*self.n_note, axis=0).reshape(-1, 1, minibatch_size, 1)
+        #self.attackTimes = np.repeat(self.attackTimes, self.ratio, axis=0).reshape(-1, minibatch_size, 1)
+
+        #pad_amount = np.abs(int(2048// 2)) # Symmetric even padding like librosa.
+        #pads = [[0, 0] for _ in range(2)]
+        #pads[1] = [pad_amount, pad_amount]
+
+        #self.pads = pads
 
         self.prev = None
         self.prev_v = None
@@ -52,7 +115,7 @@ class DataGeneratorPickles(Sequence):
         self.indices = np.arange(self.f0.shape[0])
 
     def __len__(self):
-        return int(self.f0.shape[0]/self.batch_size)
+        return int(self.f0.shape[0]/(self.batch_size//self.minibatch_size))
 
     def __call__(self):
         for i in range(self.__len__()):
@@ -62,17 +125,41 @@ class DataGeneratorPickles(Sequence):
 
     def __getitem__(self, idx):
 
-        indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
+        indices = self.indices[idx * (self.batch_size//self.minibatch_size):(idx + 1) * (self.batch_size//self.minibatch_size)]
 
-        if self.prev != self.f0[indices[0]] or self.prev_v != self.velocities[indices[0]]:
+        if self.prev != self.f0[indices[0], 0] or self.prev_v != self.velocities[indices[0], 0]:
             self.model.reset_states()
 
-        self.prev = self.f0[indices[0]]
-        self.prev_v = self.velocities[indices[0]]
+        self.prev = self.f0[indices[0], 0]
+        self.prev_v = self.velocities[indices[0], 0]
 
-        inputs = [self.f0[indices], self.k[indices], self.velocities[indices]]
-        targets = {'output_1': self.partials[indices].reshape(self.batch_size, 6), 'output_2': self.B[indices].reshape(self.batch_size, -1),
-                   'output_3': self.S[indices].reshape(self.batch_size, -1), 'output_4': self.rms[indices].reshape(self.batch_size, -1), 'output_5': self.alfas[indices].reshape(self.batch_size, -1),
-                   'output_6': self.attackTimes[indices]}
+        S = tf.pad(self.S[indices, :, :].reshape(1,-1), self.pads, mode='CONSTANT', constant_values=0)
+        self.phase = tf.math.angle(tf.signal.stft(S, frame_length=2048, frame_step=2048, pad_end=True))
+
+        inputs = [self.f0[indices], self.k[indices], self.velocities[indices], self.B[indices], self.attackTimes[indices]]
+        
+        if self.stage == 'B':
+            targets = {'output_1': self.partials[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 6)}
+        elif self.stage == 'P':
+                targets = {'output_1': self.phase,
+                           'output_2': self.S[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, self.steps)}
+
+        elif self.stage == 'S':
+                targets = {'output_1': self.S[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, self.steps)}
+
+        elif self.stage == 'A':
+            targets = {
+                   'output_1': self.S[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, self.steps),
+                   'output_2': self.rms[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 1),
+                   'output_3': self.alfas[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 1),
+                   'output_4': self.amps[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 6)
+                   }
+        else:
+            targets = {'output_1': self.partials[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 6),
+                   'output_2': self.phase,
+                   'output_3': self.S[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, self.steps),
+                   'output_4': self.rms[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 1),
+                   'output_5': self.alfas[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 1),
+                   'output_6': self.amps[indices].reshape(self.batch_size//self.minibatch_size, self.minibatch_size, 6)}
 
         return (inputs, (targets))
---
+++ Code/HarmonicModule/Layers.py
@@ -0,0 +1,330 @@
+import tensorflow as tf
+import math as m
+import numpy as np
+
+
+class InharmonicLayer(tf.keras.layers.Layer):
+    def __init__(self, harmonics=32, batch_size=1, num_frames=1, Fs=24000, trainable=True, type=tf.float32):
+        super(InharmonicLayer, self).__init__()
+        self.type = type
+        self.twopi = tf.constant(2 * m.pi, dtype=self.type)
+        self.Fs = Fs
+        self.batch_size = batch_size
+        self.num_frames = num_frames
+        self.trainable = trainable
+        self.n = tf.expand_dims(tf.linspace(1, harmonics, harmonics), axis=0)
+        self.n = tf.repeat(self.n, self.batch_size, axis=0)
+        self.n = tf.cast(self.n, dtype=self.type)
+        self.ones = tf.ones((self.batch_size, harmonics))
+
+        # self.linear = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(8, name='B', trainable=self.trainable, dtype=self.type))
+        # self.linearOut = tf.keras.layers.Dense(1, activation='sigmoid', name='Bout', trainable=self.trainable, dtype=self.type)
+
+        self.correction = tf.Variable(tf.zeros((1, 1, 1)), trainable=trainable)
+
+    def __call__(self, freq_inputs, B_inputs):
+        # f_n = tf.divide(freq_inputs, self.max_)  # BxD
+        # B = self.linear(f_n)  # BxU
+        # B = self.linearOut(B)  # Bx1
+
+        B = tf.reshape(B_inputs, [self.batch_size, 1, 1])
+        #####law3
+        n2 = tf.pow(self.n, 2)  # BxH
+
+        corr = self.correction
+
+        corr = tf.clip_by_value(corr, 0., 10.)
+        corr = tf.repeat(corr, self.batch_size, axis=0)
+        Bn = tf.keras.layers.Multiply()([corr, B])
+        Bn = tf.keras.layers.Multiply()([n2, Bn])  # BxH
+        Bn = tf.keras.layers.Add()([self.ones, Bn])  # BxH
+
+        final_n = tf.keras.layers.Multiply()([self.n, Bn])  # BxH
+        final_n = tf.reshape(final_n, [self.batch_size // self.num_frames, self.num_frames, -1])
+        partials = tf.keras.layers.Multiply()([final_n, freq_inputs])  # BxDxH
+
+        return partials, final_n
+
+
+class DeltaLayer(tf.keras.layers.Layer):
+    def __init__(self, batch_size=1, Fs=24000, trainable=True, num_frames=2400, type=tf.float32):
+        super(DeltaLayer, self).__init__()
+        self.type = type
+        self.num_frames = num_frames
+        self.twopi = tf.constant(2 * m.pi, dtype=self.type)
+        self.Fs = Fs
+        self.batch_size = batch_size
+        self.trainable = trainable
+
+        self.linear_t2 = tf.keras.layers.TimeDistributed(
+            tf.keras.layers.Dense(8, name='t2', input_shape=(self.batch_size, self.num_frames, 1),
+                                  trainable=self.trainable,
+                                  dtype=self.type))  # 128
+        self.linear_t2_1 = tf.keras.layers.Dense(8, name='t2_1', activation='gelu',
+                                                 input_shape=(self.batch_size, self.num_frames, 1),
+                                                 trainable=self.trainable, dtype=self.type)
+        # self.t2Norm = tf.keras.layers.BatchNormalization(trainable=self.trainable)
+        self.t2Norm = tf.keras.layers.BatchNormalization()
+
+        self.linearOut_t2 = tf.keras.layers.Dense(1, activation='tanh', name='t2out', trainable=self.trainable,
+                                                  dtype=self.type)
+
+    def __call__(self, vel_inputs):
+        # all_inp = tf.keras.layers.Concatenate(axis=-1)([f_n, vel_inputs])  # Bx2
+
+        t2 = self.linear_t2(vel_inputs)
+        t2 = self.linear_t2_1(t2)
+        t2 = self.t2Norm(t2)
+        t2 = self.linearOut_t2(t2)
+
+        return t2
+
+
+class SinuosoidsGenerator(tf.keras.layers.Layer):
+    def __init__(self, batch_size, harmonics=24, num_steps=24, num_frames=2400, Fs=24000, type=tf.float32):
+        """
+        Time Varying feedforward delay line
+
+        Args:
+            max_delay (int, optional): Maximum length of delay line in samples. Defaults to 40000.
+            channels (int, optional): Number of channels or audio. Defaults to 1.
+        """
+        super(SinuosoidsGenerator, self).__init__()
+        self.type = type
+        self.twopi = tf.constant(2 * m.pi, dtype=self.type)
+        self.Fs = Fs
+        self.batch_size = batch_size
+        self.harmonics = harmonics
+        self.num_steps = num_steps
+        self.num_frames = num_frames
+
+    def __call__(self, freqs_inputs, index_inputs):
+        ##### partials
+        f = tf.divide(freqs_inputs, self.Fs, name='divide')  # BxH
+        x = tf.multiply(self.twopi, f, name='mul')  # BxH
+        t = tf.ones((self.batch_size // self.num_frames, self.num_frames, self.num_steps, self.harmonics))  # BxTxH
+        ti = tf.constant(np.arange(self.num_steps * self.num_frames).reshape(1, self.num_frames, self.num_steps, 1),
+                         dtype=self.type)
+
+        t = tf.multiply(t, ti)  # BxTxH
+
+        k = tf.expand_dims(index_inputs, axis=-1)  # Bx1x1
+        k = tf.multiply(k, self.num_steps)
+        t = t + k[:, :1, :, :]
+        x = tf.expand_dims(x, axis=2)  # Bx1xH
+        partials = tf.multiply(x, t, name='mul2')  # BxTxH
+
+        return partials, f, t
+
+
+class DecayLayer(tf.keras.layers.Layer):
+    def __init__(self, batch_size, harmonics=24, Fs=24000, trainable=True, num_frames=2400, g=0, phan=False,
+                 type=tf.float32):
+        """
+        Time Varying feedforward delay line
+
+        Args:
+            max_delay (int, optional): Maximum length of delay line in samples. Defaults to 40000.
+            channels (int, optional): Number of channels or audio. Defaults to 1.
+        """
+        super(DecayLayer, self).__init__()
+        self.batch_size = batch_size
+        self.type = type
+        self.num_frames = num_frames
+        self.twopi = tf.constant(2 * m.pi, dtype=self.type)
+        self.Fs = Fs
+        self.harmonics = harmonics
+        self.trainable = trainable
+        self.p = 2
+        self.units = self.harmonics * self.p
+        self.g = g
+        self.phan = phan
+
+        self.alfaIn = tf.keras.layers.TimeDistributed(
+            tf.keras.layers.Dense(self.units // 2, name='alfaIn',
+                                  batch_input_shape=(self.batch_size // self.num_frames, self.num_frames, 2),
+                                  trainable=self.trainable, dtype=self.type))  #
+        self.alfa = tf.keras.layers.LSTM(self.units // 2, stateful=True, return_sequences=True, name='alfa',
+                                         trainable=self.trainable, dtype=self.type)
+
+        self.alfaDenseGelu = tf.keras.layers.Dense(self.units // 2, activation='gelu', name='alfa3',
+                                                   trainable=self.trainable, dtype=self.type)  ##
+        self.alfaOut = tf.keras.layers.Dense(self.units, name='alfaOut', trainable=self.trainable, dtype=self.type)
+        # self.alfaNorm = tf.keras.layers.BatchNormalization(trainable=self.trainable)
+        self.alfaNorm = tf.keras.layers.BatchNormalization()
+
+        # self.alfaNormPre = tf.keras.layers.BatchNormalization(trainable=self.trainable)
+        self.alfaNormPre = tf.keras.layers.BatchNormalization()
+
+        self.Att = tf.keras.layers.MultiHeadAttention(1, 8)  # , attention_axes=[2])
+
+        self.b = tf.Variable(tf.random.normal([4]), trainable=self.trainable)
+
+        # self.alfas = tf.Variable(tf.zeros([self.units]), trainable=False)
+        self.perm = tf.keras.layers.Permute((2, 1))
+
+    def compute_decay_rate(self, b0, b1, b2, b3, t_s):
+        decay_rate = tf.add(b0, b1)
+        decay_rate = tf.add(decay_rate, b2)
+        decay_rate = tf.add(decay_rate, b3)
+        decay_rate = tf.abs(decay_rate)  # BxDxH
+        decay_rate = tf.expand_dims(decay_rate, axis=2)  # Bx1xH
+        decay_rate = tf.multiply(decay_rate, t_s)
+        return decay_rate
+
+    def compute_free_long(self, alfas):
+
+        alfa_even = tf.pow(alfas[0][:, :, :, self.g:], 2.)
+        alfa_even2 = tf.pow(alfas[1][:, :, :, self.g:], 2.)
+        return [alfa_even, alfa_even2]
+
+    def compute_phantoms(self, alfas):
+        alfa_odd = []
+        alfa_odd2 = []
+        alfa_odd3 = []
+
+        for i in range(self.g, alfas[0].shape[-1] - 1):
+            alfa_odd.append(tf.multiply(i * alfas[0][:, :, :, i], (i + 1) * alfas[0][:, :, :, i + 1]))
+            alfa_odd.append(tf.multiply(i * alfas[0][:, :, :, i], (i + 1) * alfas[0][:, :, :, i + 1]))
+
+            alfa_odd2.append(tf.multiply(i * alfas[1][:, :, :, i], (i + 1) * alfas[1][:, :, :, i + 1]))
+            alfa_odd2.append(tf.multiply(i * alfas[1][:, :, :, i], (i + 1) * alfas[1][:, :, :, i + 1]))
+
+            alfa_odd3.append(tf.multiply(i * alfas[0][:, :, :, i], (i + 1) * alfas[1][:, :, :, i + 1]))
+            alfa_odd3.append(tf.multiply(i * alfas[0][:, :, :, i], (i + 1) * alfas[1][:, :, :, i + 1]))
+
+        alfa_odd = tf.transpose(tf.convert_to_tensor(alfa_odd), perm=[1, 2, 3, 0])
+        alfa_odd2 = tf.transpose(tf.convert_to_tensor(alfa_odd2), perm=[1, 2, 3, 0])
+        alfa_odd3 = tf.transpose(tf.convert_to_tensor(alfa_odd3), perm=[1, 2, 3, 0])
+        return [alfa_odd, alfa_odd2, alfa_odd3]
+
+    def __call__(self, vel_inputs, f, t, K, partials, t_even, t_odd, attackTime):
+        # all_inp = tf.keras.layers.Concatenate(axis=-1)([f_n, vel_inputs])  # BxDx2
+
+        attackTime_t = tf.multiply(attackTime, t[:, :, :, 0])  # BxTx1
+        attackTime_t = tf.math.minimum(attackTime_t, 1.)
+
+        b_v = tf.abs(tf.nn.gelu(self.b))
+        # b_v = tf.abs(self.b)
+        # b_v = tf.multiply(b_v, [1., 1., 500., 1.])
+
+        all_inp = tf.keras.layers.Concatenate(axis=-1)([vel_inputs, K])  # Bx3
+        alfa_v = self.alfaIn(all_inp)  # Bx1xH
+        alfa_v = self.alfaNormPre(alfa_v)  # BxDxH
+        alfa_v = self.alfa(alfa_v)  # BxDxH
+
+        alfa_v = self.alfaDenseGelu(alfa_v)  # BxDxH
+        alfa_v = self.alfaNorm(alfa_v)  # BxDxH
+
+        alfa_v = self.perm(alfa_v)  # BxHxD
+        alfa_v = self.Att(alfa_v, alfa_v)
+        alfa_v = self.perm(alfa_v)  # BxHxD
+
+        alfa_v = self.alfaOut(alfa_v)  # BxDxH
+        alfa_v = tf.abs(alfa_v)
+
+        alfa_v = tf.clip_by_value(alfa_v, 0., 1.)
+        # b_v = tf.clip_by_value(b_v, 0.0000001, 1.)
+
+        # alfa_v = tf.multiply(alfa_v, attackTime_t)
+        # self.alfas.assign(alfa_v[0, 0, :])
+
+        alfa_v, alfa_h = tf.split(alfa_v, [self.harmonics, self.harmonics], axis=-1)
+
+        b0, b1, b2, b3 = tf.split(b_v, 4, axis=-1)
+        f3 = tf.pow(f[1], 3)
+        t_s = tf.divide(t, self.Fs)
+        f_sqrt = tf.sqrt(f[1])
+
+        b3_ = tf.multiply(f[1], b3)  # BxDxH #####
+        b2_ = tf.multiply(f3, b2)  # BxDxH #####
+        b1_ = tf.multiply(b1, f_sqrt)
+
+        alfa_h = tf.expand_dims(alfa_h, axis=2)  # Bx1xH
+        decay_rates_h = self.compute_decay_rate(b0, b1_, b2_, b3_, t_s)
+        decay_rates_h = tf.multiply(decay_rates_h, m.pi)
+        decay_h = tf.math.exp(-decay_rates_h)
+        decay_h = tf.multiply(decay_h, alfa_h)
+
+        f3 = tf.pow(f[0], 3)
+        f_sqrt = tf.sqrt(f[0])
+
+        b3_ = tf.multiply(f[0], b3)  # BxH #####
+        b2_ = tf.multiply(f3, b2)  # BxH #####
+        b1_ = tf.multiply(b1, f_sqrt)  # BxH #####
+
+        alfa_v = tf.expand_dims(alfa_v, axis=2)  # Bx1xH
+        decay_rates_v = self.compute_decay_rate(b0, b1_, b2_, b3_, t_s)  # BxTxH #####
+        decay_rates_v = tf.multiply(decay_rates_v, m.pi)
+        decay_v = tf.math.exp(-decay_rates_v)
+        decay_v = tf.multiply(decay_v, alfa_v)
+
+        if self.phan:
+            ## free resposne
+            f_even = tf.divide(partials[0], self.Fs, name='divide')  # BxH
+            f_even2 = tf.divide(partials[1], self.Fs, name='divide')  # BxH
+            f_odd = tf.divide(partials[2], self.Fs, name='divide')  # BxH
+            f_odd2 = tf.divide(partials[3], self.Fs, name='divide')  # BxH
+            f_odd3 = tf.divide(partials[4], self.Fs, name='divide')  # BxH
+
+            alfas_even = self.compute_free_long([decay_v, decay_h])  # BxTxH #####
+
+            f3 = tf.pow(f_even, 3)
+            f_sqrt = tf.sqrt(f_even)
+            b3_ = tf.multiply(f_even, b3)  # BxH #####
+            b2_ = tf.multiply(f3, b2)  # BxH #####
+            b1_ = tf.multiply(b1, f_sqrt)  # BxH #####
+            decay_rates_even = self.compute_decay_rate(b0, b1_, b2_, b3_, t_even)
+            decay_rates_even = tf.multiply(decay_rates_even, m.pi)
+            decay_even = tf.math.exp(-decay_rates_even)
+            decay_even = tf.multiply(decay_even, alfas_even[0])
+
+            f3 = tf.pow(f_even2, 3)
+            f_sqrt = tf.sqrt(f_even2)
+            b3_ = tf.multiply(f_even2, b3)  # BxH #####
+            b2_ = tf.multiply(f3, b2)  # BxH #####
+            b1_ = tf.multiply(b1, f_sqrt)  # BxH #####
+            decay_rates_even2 = self.compute_decay_rate(b0, b1_, b2_, b3_, t_even)
+            decay_rates_even2 = tf.multiply(decay_rates_even2, m.pi)
+            decay_even2 = tf.math.exp(-decay_rates_even2)
+            decay_even2 = tf.multiply(decay_even2, alfas_even[1])
+
+            ## coupling
+            alfas_odd = self.compute_phantoms([decay_v, decay_h])  # BxTxH #####
+
+            f3 = tf.pow(f_odd, 3)
+            f_sqrt = tf.sqrt(f_odd)
+            b3_ = tf.multiply(f_odd, b3)  # BxH #####
+            b2_ = tf.multiply(f3, b2)  # BxH #####
+            b1_ = tf.multiply(b1, f_sqrt)  # BxH #####
+            decay_rates_odd = self.compute_decay_rate(b0, b1_, b2_, b3_, t_odd)
+            decay_rates_odd = tf.multiply(decay_rates_odd, m.pi)
+            decay_odd = tf.math.exp(-decay_rates_odd)
+            decay_odd = tf.multiply(decay_odd, alfas_odd[0])
+
+            f3 = tf.pow(f_odd2, 3)
+            f_sqrt = tf.sqrt(f_odd2)
+            b3_ = tf.multiply(f_odd2, b3)  # BxH #####
+            b2_ = tf.multiply(f3, b2)  # BxH #####
+            b1_ = tf.multiply(b1, f_sqrt)  # BxH #####
+            decay_rates_odd2 = self.compute_decay_rate(b0, b1_, b2_, b3_, t_odd)
+            decay_rates_odd2 = tf.multiply(decay_rates_odd2, m.pi)
+            decay_odd2 = tf.math.exp(-decay_rates_odd2)
+            decay_odd2 = tf.multiply(decay_odd2, alfas_odd[1])
+
+            f3 = tf.pow(f_odd3, 3)
+            f_sqrt = tf.sqrt(f_odd3)
+            b3_ = tf.multiply(f_odd3, b3)  # BxH #####
+            b2_ = tf.multiply(f3, b2)  # BxH #####
+            b1_ = tf.multiply(b1, f_sqrt)  # BxH #####
+            decay_rates_odd3 = self.compute_decay_rate(b0, b1_, b2_, b3_, t_odd)
+            decay_rates_odd3 = tf.multiply(decay_rates_odd3, m.pi)
+            decay_odd3 = tf.math.exp(-decay_rates_odd3)
+            decay_odd3 = tf.multiply(decay_odd3, alfas_odd[2])
+
+            phantom_decays = 0.1 * [decay_even, decay_even2, decay_odd, decay_odd2, decay_odd3]
+
+            return decay_v, decay_h, attackTime_t, phantom_decays  # BxTxH
+        else:
+
+            return decay_v, decay_h, attackTime_t
